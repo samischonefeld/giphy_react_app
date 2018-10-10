b62f5ccb2532c8743e1c85b62ac099a9f63afb14
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-10-10 14:30:30 -0400

    works in one component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(props){
+  super(props)
+  this.state = {
+    giphyname: '',
+    giphyimage: '',
+    search: '',
+  }
+}
+
+fetchAPI(res){
+  let search = this.state.search
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=tt59MI4q4zQA4P24YXAQEN6iL7GQq3EN&q=${search}&limit=25&offset=0&rating=G&lang=en`
+  fetch(url)
+    .then( res => res.json() )
+    .then( res =>{
+      this.setState(prevState => ({
+        giphyname: res.data[0].title,
+        giphyimage: res.data[0].embed_url,
+      }))
+      })
+    }
+
+ handleSubmit(e){
+  e.preventDefault()
+  this.fetchAPI()
+  }
+
+handleChange(e){
+  this.setState(
+  {search: e.target.value})
+}
+
   render() {
+    let image = this.state.giphyimage
+    console.log(image)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Giphy Creator</h1>
+        <form>
+          <input onChange={(e) => this.handleChange(e)}/>
+          <button onClick={(e) => this.handleSubmit(e)}>
+          Submit
+          </button>
+        </form>
+      <h1> {this.state.giphyname}</h1>
+      <iframe src={ image } />
       </div>
     );
   }
